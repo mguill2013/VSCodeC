4873ed58a175203d53a40290af1ed180cf3f6721
GreenSaguaro-RedLynx <mguill2013@gmail.com>
2025-07-16 07:47:18 -0400

    alternator
    This is a simple program I wrote to practice my use of static variables.
    The alternator function alternates between 0 and 1, and i've used a for loop iterating 5 times to showcase the output.

---
+++ alternator.c
@@ -0,0 +1,25 @@
+/*
+ * Program: alternator.c
+ * Author: Michael Guillory
+ * Date: 5/21/2025
+ * Course: EGN 3211: Engineering Analysis and Computation
+
+ * Description: This program implements a simple alternator function that toggles its state
+ * using a static variable. Each time the function is called, it alternates between returning 0 and 1.
+ * The main function demonstrates this by calling the alternator function five times and printing the result. 
+*/
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        printf("Alternator state: %d\n", alternator());
+    }
+    return 0;
+}
+
+int alternator() {
+    static int state = 0;
+    state = 1 - state;
+    return state;
+}
